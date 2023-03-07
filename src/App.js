e2e1e546b65a50e605f24db2a2b6343f1eaa2553
alexander.joey@gmail.com <learnacademy@LEARNs-MacBook-Air.local>
2023-03-07 11:13:38 -0800

    Alert to show whether player 1 or player 2 won

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(0)
   
-  const checkTurn = (index ) => {
-    if (squares[index] === null) {
+  const checkTurn = (index) => {
+    if(calculateWinner(squares) !== null){
+      console.log(calculateWinner(squares))
+     const winner = calculateWinner(squares) === "X" ? "Player One won(O)" : "Player Two won(X)"
+       alert(winner)
+
+    }else if (squares[index] === null) {
       // Draws X if it's player 1's turn else draws O
       const current = turn === 0 ? "X" : "O"
       // Updating the data state
@@ -19,6 +24,26 @@ const App = () => {
       setTurn(turn === 0 ? 1 : 0)
       console.log(turn)
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
   
   return (
@@ -32,6 +57,8 @@ const App = () => {
           value={value}
           index={index}
           checkTurn={checkTurn}
+          calculateWinner={calculateWinner}
+
            />
         )
       })}
